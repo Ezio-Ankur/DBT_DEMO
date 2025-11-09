63ac4a83ccf4be8f1be568ef121eb86212db133e
ankurranjan08@gmail.com <ankurranjan08@gmail.com>
2025-11-09 06:14:28 +0000

    date_dimension

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -5,16 +5,10 @@ select
 to_timestamp(STARTED_AT) as timestamp ,
 date(to_timestamp(STARTED_AT)) as date_started_at,
 hour(to_timestamp(STARTED_AT)) as hour,
-case 
-when dayname(to_timestamp(STARTED_AT)) in ('Sat','Sun') 
-then 'WEEKEND'
-else 'BUSINESS'
-END AS DAY_TYPE,
-case when month(to_timestamp(STARTED_AT)) in (12,1,2) then 'WINTER'
-WHEN month(to_timestamp(STARTED_AT)) in (3,4,5) then 'SPRING'
-WHEN month(to_timestamp(STARTED_AT)) in (6,7,8) then 'SUMMER'
-ELSE 'AUTUMN'
-END AS STATION_OF_YEAR
+
+{{day_type('STARTED_AT')}} as day_type,
+
+{{get_season('STARTED_AT')}}  AS STATION_OF_YEAR
 
 from 
 {{ source('demo', 'bike') }}
